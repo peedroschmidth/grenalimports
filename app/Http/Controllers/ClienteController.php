c55c9633c0e6c3c7ac057e70c764cc030d81b485
Pedro Schmidt <pedroschmidthpedro@gmail.com>
2021-11-16 17:09:08 -0300

    commitei sem middleware

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -13,10 +13,7 @@ class ClienteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     public function index()
     {
