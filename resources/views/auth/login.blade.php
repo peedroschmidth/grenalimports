535b77ed220955e6cc65a9b12ce9d10e31f7278e
Pedro Schmidt <pedroschmidthpedro@gmail.com>
2021-12-03 11:03:02 -0300

    ultima versão

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <h3>Página de Login</h3>
+                <h3><center>Página de Login<center></h3>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
